221feb44b9255e454fc273702b331aa201941f73
Martin Szuc <mszuc@redhat.com>
2024-11-03 16:00:53 +0100

    fix: events

--- events.py
+++ events.py
@@ -6,10 +6,8 @@ STATIC_EVENTS = {
     2: {"time": "01:30", "message": "First Flagbearer creep spawning in 30 seconds!"},
     3: {"time": "02:00", "message": "First Flagbearer creep has spawned!"},
     4: {"time": "20:00", "message": "First Tormentor has spawned!"},
-    5: {"time": "02:00", "message": "Glyph will be available in 30 seconds!"},
-    6: {"time": "02:30", "message": "Glyph is now available!"},
-    7: {"time": "00:20", "message": "Thomas stinks"},
-    8: {"time": "00:10", "message": "Dano je kokot!"},
+    5: {"time": "02:30", "message": "Glyph will be available in 30 seconds!"},
+    6: {"time": "03:00", "message": "Glyph is now available!"},
 }
 
 # Periodic events defined with unique IDs, start time, interval, end time, and message
@@ -18,5 +16,5 @@ PERIODIC_EVENTS = {
     2: {"start_time": "06:00", "interval": "07:00", "end_time": "99:00", "message": "XP Runes spawning in 60 seconds!"},
     3: {"start_time": "19:00", "interval": "10:00", "end_time": "99:00", "message": "Tormentor spawning in 60 seconds!"},
     4: {"start_time": "04:30", "interval": "05:00", "end_time": "99:00", "message": "Siege Creep spawning in 30 seconds!"},
-    5: {"start_time": "07:00", "interval": "07:00", "end_time": "99:00", "message": "Smoke will restock in 1 minute!"},
+    # 5: {"start_time": "07:00", "interval": "07:00", "end_time": "99:00", "message": "Smoke will restock in 1 minute!"},
 }
