73c49fe76eb3cf6d10f9b2527635b3811c7e20c8
Martin Szuc <mszuc@redhat.com>
2024-11-09 03:37:12 +0100

    chore: unused code

--- event_manager.py
+++ event_manager.py
@@ -17,23 +17,6 @@ class EventsManager:
     def __init__(self):
         self.session = SessionLocal()
 
-    def initialize_guild_events(self, guild_id):
-        """Copy base template events for a new guild."""
-        template_static_events = self.session.query(StaticEvent).filter_by(guild_id=None).all()
-        template_periodic_events = self.session.query(PeriodicEvent).filter_by(guild_id=None).all()
-
-        for event in template_static_events:
-            guild_event = StaticEvent(guild_id=guild_id, mode=event.mode, time=event.time, message=event.message)
-            self.session.add(guild_event)
-
-        for event in template_periodic_events:
-            guild_event = PeriodicEvent(guild_id=guild_id, mode=event.mode, start_time=event.start_time,
-                                        interval=event.interval, end_time=event.end_time, message=event.message)
-            self.session.add(guild_event)
-
-        self.session.commit()
-        logger.info(f"Base template events copied for guild_id: {guild_id}")
-
     def get_static_events(self, guild_id, mode='regular'):
         """Retrieve all static events for a guild and mode."""
         events = self.session.query(StaticEvent).filter_by(guild_id=str(guild_id), mode=mode).all()
